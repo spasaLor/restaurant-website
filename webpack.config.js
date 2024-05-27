31621c425f879faca950cbd8d02dbebbba9fe060
Lorenzo Spadaro Sapari <98901805+spasaLor@users.noreply.github.com>
2024-05-27 20:57:52 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ],
+  },
+  plugins:[
+    new htmlPlugin({
+        template:'./src/index.html',
+        filename:'index.html',
+        inject:'head',
+    })
+  ]
+};
